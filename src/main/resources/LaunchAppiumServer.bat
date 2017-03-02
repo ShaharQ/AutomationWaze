2ec1820972c03b682a7cd785165ed3b99bc47ce8
moshik177 <moshik.kalash@qualitest.co.il>
2017-03-02 16:49:41 +0200

    fixed process on windows

--- src/main/resources/LaunchAppiumServer.bat
+++ src/main/resources/LaunchAppiumServer.bat
@@ -1,2 +1,2 @@
-cd ~/IdeaProjects/WazeAutomationAndroid/src/main/resources/
+cd C:\Jenkins\workspace\WazeQualitest-02-AutomationGradle\src\main\resources\LaunchAppiumServer.bat
 java -jar selenium-server-standalone-3.0.1.jar -role hub -hubConfig DefaultHub.json -debug
